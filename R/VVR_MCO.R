60ec1ec77109125b1a7725a610a5bc35b7660802
GuillaumePressiat <guillaume.pressiat@gmail.com>
2025-04-28 11:28:50 +0200

    Fix #60

--- R/VVR_MCO.R
+++ R/VVR_MCO.R
@@ -305,7 +305,8 @@ vvr_ghs_supp <- function(rsa,
   if (is.null(prudent)) {
     rsa_2 <- rsa %>%
       dplyr::filter(substr(noghs,1,1) != 'I') %>%
-      dplyr::mutate(cprudent = dplyr::case_when(anseqta == "2024" ~ 0.993 * 1.0042,
+      dplyr::mutate(cprudent = dplyr::case_when(anseqta == "2025" ~ 0.993 * 1.0035,
+                                                anseqta == "2024" ~ 0.993 * 1.0042,
                                                 anseqta == "2023" ~ 0.993 * 1.0023,
                                                 anseqta == "2022" ~ 0.993 * 1.0013,
                                                 anseqta == "2021" ~ 0.993 * 1.0019, 
@@ -329,7 +330,8 @@ vvr_ghs_supp <- function(rsa,
                                                                                             t_bas), rec_totale = rec_bee) %>%
       bind_rows(rsa %>%
                   dplyr::filter(substr(noghs,1,1) == 'I') %>%
-                  dplyr::mutate(cprudent = dplyr::case_when(anseqta == "2024" ~ 0.993,
+                  dplyr::mutate(cprudent = dplyr::case_when(anseqta == "2025" ~ 0.993,
+                                                            anseqta == "2024" ~ 0.993,
                                                             anseqta == "2023" ~ 0.993, 
                                                             anseqta == "2022" ~ 0.993, 
                                                             anseqta == "2021" ~ 0.993, 
