12a23b4c442ba4bb313fa84a4f698f1256de6b34
GuillaumePressiat <guillaume.pressiat@gmail.com>
2025-05-12 16:55:53 +0200

    Update VVR_MCO.R

--- R/VVR_MCO.R
+++ R/VVR_MCO.R
@@ -352,6 +352,7 @@ vvr_ghs_supp <- function(rsa,
       dplyr::filter(substr(noghs,1,1) != 'I') %>%
       
       dplyr::mutate(cprudent = dplyr::case_when(
+        anseqta == "2025" ~ prudent * 1.0035,
         anseqta == "2024" ~ prudent * 1.0042,
         anseqta == "2023" ~ prudent * 1.0023,
         anseqta == "2022" ~ prudent * 1.0013,
